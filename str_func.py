0c79031eee31b26b7f2afc0cd9ef00dab258021f
Denis Rusinov <denisrusinov30.gmail.com>
2024-04-03 21:03:35 +0500

    added str_func.py with 2 function and docstrig

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
 def text_in_capital_letters(text=str(input())):
+    """функия делающая все буквы заглавными"""
     return text.upper()
+
+
+def capital_letters_of_words(string):
+    """функия делающая каждое слово предложения с заглавной буквы"""
+    result = ' '.join(word.capitalize() for word in string.split())
+    return result
+
+
+string = 'big world'
+print(capital_letters_of_words(string))
